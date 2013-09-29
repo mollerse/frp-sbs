a1a028485d9740a3241e3e828be01f276892324c
Stian Veum Møllersen <stian.moellersen@gmail.com>
2013-09-29 21:37:09 +0200

    Wrote the reactive implementation

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
+var fs = require('fs');
 var ecstatic = require('ecstatic');
+var qs = require('querystring');
 
 var staticd = ecstatic({
     root: __dirname + '/static',
@@ -7,6 +9,14 @@ var staticd = ecstatic({
 });
 
 var server = http.createServer(function(req, res) {
+    if(/^\/records$/.test(req.url)) {
+        res.setHeader('content-type', 'application/json');
+        return fs.createReadStream(__dirname+"/records.json").pipe(res);
+    }
+    if(/^\/records\/new$/.test(req.url)) {
+        res.setHeader('content-type', 'application/json');
+        return req.pipe(res);
+    }
     return staticd(req, res);
 });
 
